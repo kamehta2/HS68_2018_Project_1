5f071cc8a4619b400f4c41d57d3fc07b5f4ab794
Kinjal <kamehta2@dons.usfca.edu>
2018-07-26 00:55:35 -0700

    Refactored code to include Class NormCheck for re-usability and ease of usage
    Added Class NormCheck
    Added two new functions
    
    Tested code with sample inputs

--- hs628_tools/normalization_tool.py
+++ hs628_tools/normalization_tool.py
@@ -2,37 +2,48 @@ import numpy as np
 import sklearn as sk
 from sklearn import preprocessing
 
-#alldata = np.genfromtxt('heart.csv', delimiter=',', skip_header=1)
-#alldata = np.genfromtxt('data.csv', delimiter=',', skip_header=1)
-alldata = np.genfromtxt('college.csv', delimiter=',', skip_header=1)
 
-rowwise_missing_entries = len(alldata) - np.sum(alldata==alldata, axis=0)
+class NormCheck:
+    def __init__(self, csv_file, cutoff=0.3, alldata1=None):
+        self.csv_file = csv_file
+        self.cutoff = cutoff
+        self.alldata1 = alldata1
 
-data_nocategory_columns = rowwise_missing_entries < len(alldata)
-alldata1 = alldata[:, data_nocategory_columns]
+    def find_percent_diff(self):
+        alldata = np.genfromtxt(self.csv_file, delimiter=',', skip_header=1)
+        rowwise_missing_entries = len(alldata) - np.sum(alldata == alldata, axis=0)
+        data_nocategory_columns = rowwise_missing_entries < len(alldata)
+        self.alldata1 = alldata[:, data_nocategory_columns]
+        allmean = self.alldata1.mean(axis=0)
+        allmeanmin = allmean.min()
+        allmeanmax = allmean.max()
+        percent_diff = allmeanmin * 100 / float(allmeanmax)
+        return percent_diff
 
-allmean = alldata1.mean(axis=0)
+    def get_input(self, percent_diff):
+        if percent_diff < self.cutoff:
+            rescale = raw_input ("This dataset should be normalized. Do you want to proceed with normalization?"+"\n"+"YES/NO"+"\n")
+            return rescale
+        else:
+            return "This dataset may not need normalization."
 
-allmeanmin = allmean.min()
-allmeanmax = allmean.max()
-allmeanrange = allmeanmax - allmeanmin
-percent_diff = allmeanmin * 100/ float(allmeanmax)
+    def compare(self, rescale):
+        if rescale.upper() == "YES":
+            alldata_normalized = preprocessing.normalize(self.alldata1)
+            alldata_normalized = alldata_normalized
+            return alldata_normalized
+        else:
+            return "You have selected not to normalize your dataset."
 
-def getinput():
-    if percent_diff < 0.3:
-        rescale = raw_input ("This dataset should be normalized. Do you want to proceed with normalization?"+"\n"+"Yes/No"+"\n")
-        return rescale
-    else:
-        print "This dataset may not need normalization."
 
-normalization_input = getinput()
+if __name__ == '__main__':
 
-def compare():
-    if normalization_input.upper() == "YES":
-        alldata_normalized = preprocessing.normalize(alldata1)
-        return alldata_normalized
-    else:
-        print "You have selected not to normalize your dataset."
+    Normalization = NormCheck("College.csv",0.3)
+    #Normalization = NormCheck(College.csv)
+    #Normalization = NormCheck(heart.csv, 0.4)
+    #Normalization = NormCheck(data.csv)
 
-normalized_data = compare()
-normalized_data
+    percentage = Normalization.find_percent_diff()
+    rescale_input = Normalization.get_input(percentage)
+    normalized_data = Normalization.compare(rescale_input)
+    print normalized_data
