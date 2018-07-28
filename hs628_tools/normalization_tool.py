5093a9ae47bdbedfb5c36a8fd2317e074c8d893d
Kinjal <kamehta2@dons.usfca.edu>
2018-07-28 14:03:35 -0700

    Adding Docstring to every function

--- hs628_tools/normalization_tool.py
+++ hs628_tools/normalization_tool.py
@@ -10,6 +10,10 @@ class NormCheck:
         self.alldata1 = alldata1
 
     def find_percent_diff(self):
+        """ 
+        This function reads the data from .csv file as a numpy.ndarray. It calculates mean for each column.
+        It further processes this data to return the percentage difference.
+        """
         alldata = np.genfromtxt(self.csv_file, delimiter=',', skip_header=1)
         rowwise_missing_entries = len(alldata) - np.sum(alldata == alldata, axis=0)
         data_nocategory_columns = rowwise_missing_entries < len(alldata)
@@ -20,30 +24,44 @@ class NormCheck:
         percent_diff = allmeanmin * 100 / float(allmeanmax)
         return percent_diff
 
+
     def get_input(self, percent_diff):
+        """
+        This function presents user to provide an input, if it determines that data will benefit from normalization.
+        The user input gives choice to user if they want to proceed with normalization or not.
+        If data does not need normalization, the function returns with an informative message reflecting the same.
+        """
         if percent_diff < self.cutoff:
             rescale = raw_input ("This dataset should be normalized. Do you want to proceed with normalization?"+"\n"+"YES/NO"+"\n")
             return rescale
         else:
             return "This dataset may not need normalization."
 
+
     def compare(self, rescale):
+        """
+        This function checks if user decided to proceed with normalization, and if so, goes ahead and performs normalization
+        and returns normalized dataset as a numpy.ndarray. Else, it reflects a message that the user has not selected to normalize the dataset,
+        even though the recommendation was provided to the user.
+        """
         if rescale.upper() == "YES":
             alldata_normalized = preprocessing.normalize(self.alldata1)
             alldata_normalized = alldata_normalized
             return alldata_normalized
         else:
-            return "You have selected not to normalize your dataset."
+            return "This dataset may not need normalization or you have selected not to normalize your dataset."
 
 
 if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
 
-    Normalization = NormCheck("College.csv",0.3)
-    #Normalization = NormCheck(College.csv)
-    #Normalization = NormCheck(heart.csv, 0.4)
-    #Normalization = NormCheck(data.csv)
+    normalization = NormCheck("College.csv",0.1)
+    #normalization = NormCheck(College.csv)
+    #normalization = NormCheck(heart.csv, 0.4)
+    #normalization = NormCheck(data.csv)
 
-    percentage = Normalization.find_percent_diff()
-    rescale_input = Normalization.get_input(percentage)
-    normalized_data = Normalization.compare(rescale_input)
+    percentage = normalization.find_percent_diff()
+    rescale_input = normalization.get_input(percentage)
+    normalized_data = normalization.compare(rescale_input)
     print normalized_data
