792da99d9f4561f8db88919da9eec95364b197e3
Kinjal <kamehta2@dons.usfca.edu>
2018-07-29 12:05:48 -0700

    read the data from "main", not in function

--- hs628_tools/normalization_tool.py
+++ hs628_tools/normalization_tool.py
@@ -4,17 +4,16 @@ from sklearn import preprocessing
 
 
 class NormCheck:
-    def __init__(self, csv_file, cutoff=0.3, alldata1=None):
-        self.csv_file = csv_file
+    def __init__(self, alldata, cutoff=0.3, alldata1=None):
+        self.alldata = alldata
         self.cutoff = cutoff
         self.alldata1 = alldata1
 
     def find_percent_diff(self):
         """ 
-        This function reads the data from .csv file as a numpy.ndarray. It calculates mean for each column.
+        This function takes the data as a numpy.ndarray. It calculates mean for each column.
         It further processes this data to return the percentage difference.
         """
-        alldata = np.genfromtxt(self.csv_file, delimiter=',', skip_header=1)
         rowwise_missing_entries = len(alldata) - np.sum(alldata == alldata, axis=0)
         data_nocategory_columns = rowwise_missing_entries < len(alldata)
         self.alldata1 = alldata[:, data_nocategory_columns]
@@ -23,7 +22,6 @@ class NormCheck:
         allmeanmax = allmean.max()
         percent_diff = allmeanmin * 100 / float(allmeanmax)
         return percent_diff
-
 
     def get_input(self, percent_diff):
         """
@@ -37,7 +35,6 @@ class NormCheck:
         else:
             return "This dataset may not need normalization."
 
-
     def compare(self, rescale):
         """
         This function checks if user decided to proceed with normalization, and if so, goes ahead and performs normalization
@@ -49,18 +46,21 @@ class NormCheck:
             alldata_normalized = alldata_normalized
             return alldata_normalized
         else:
-            return "This dataset may not need normalization or you have selected not to normalize your dataset."
+            return "You have selected not to normalize your dataset."
 
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
 
-    normalization = NormCheck("College.csv",0.1)
-    #normalization = NormCheck(College.csv)
-    #normalization = NormCheck(heart.csv, 0.4)
-    #normalization = NormCheck(data.csv)
-
+    alldata = np.genfromtxt("College.csv", delimiter=',', skip_header=1)
+    #alldata = np.genfromtxt("heart.csv", delimiter=',', skip_header=1)
+    #alldata = np.genfromtxt("data.csv", delimiter=',', skip_header=1)
+    
+    normalization = NormCheck(alldata,0.25)
+    #normalization = NormCheck(alldata)
+    #normalization = NormCheck(alldata, 0.4)
+    
     percentage = normalization.find_percent_diff()
     rescale_input = normalization.get_input(percentage)
     normalized_data = normalization.compare(rescale_input)
